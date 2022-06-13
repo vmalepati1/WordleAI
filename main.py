626c70b898b1b4afe1091dc77362e4c4e2673da1
vmalepati1 <malepativikas@gmail.com>
2022-06-13 00:58:02 -0400

    Successfully closing dialogue box

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
+def expand_shadow_element(element):
+  shadow_root = driver.execute_script('return arguments[0].shadowRoot.children', element)
+  return shadow_root[1]
+
+driver = webdriver.Firefox()
 driver.get("https://www.nytimes.com/games/wordle/index.html")
+
+root1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "game-app")))
+shadow_root1 = expand_shadow_element(root1)
+
+root2 = shadow_root1.find_element(By.TAG_NAME, 'game-modal')
+shadow_root2 = expand_shadow_element(root2)
+
+close_button = shadow_root2.find_element(By.TAG_NAME, 'game-icon')
+
+close_button.click()
+
