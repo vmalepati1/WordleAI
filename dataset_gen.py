6490ce9e85eb9d6f29eeee170d8462e1c63a3b1f
vmalepati1 <malepativikas@gmail.com>
2022-06-13 19:33:42 -0400

    Create dataset

--- dataset_gen.py
+++ dataset_gen.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import csv
 
 _wait = 0.5
 
@@ -16,6 +17,18 @@ def get_freq(term):
     freq = 0.0 if len(response)==0 else float(response[0]['tags'][0][2:])
     return freq
 
-with open('solution_words.txt') as words:
-    for word in words:
-        print(word.strip())
+with open('dataset.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # Write header
+    writer.writerow(['word', 'freq_rank'])
+    
+    with open('solution_words.txt') as words:
+        for word in words:
+            stripped_word = word.strip()
+            
+            freq = get_freq(stripped_word)
+            
+            writer.writerow([stripped_word, freq])
+
+
