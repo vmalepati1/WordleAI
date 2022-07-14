e563647735d573b84dc1f0756d8669fdac102811
vmalepati1 <malepativikas@gmail.com>
2022-07-14 00:26:27 -0400

    Add env class

---
+++ dqn/wordle_env.py
@@ -0,0 +1,50 @@
+from dqn.state import WordleState
+
+REWARD = 10
+
+class WordleEnv:
+
+    def __init__(self, words, max_turns, allowable_words = None):
+        self.words = words
+        self.max_turns = max_turns
+        self.allowable_words = allowable_words
+
+        if not self.allowable_words:
+            self.allowable_words = len(self.words)
+
+        self.done = True
+        self.goal_word_idx = -1
+        self.state = None
+
+    def reset(self):
+        self.state = dqn.state.new(self.max_turns)
+        self.done = False
+        self.goal_word = int(np.random.random()*self.allowable_words)
+
+    def step(self, action: int):
+        if self.done:
+            raise ValueError(
+                "You are calling 'step()' even though this "
+                "environment has already returned done = True. You "
+                "should always call 'reset()' once you receive 'done = "
+                "True' -- any further steps are undefined behavior."
+            )
+        self.state = dqn.state.update(state=self.state,
+                                        word=self.words[action],
+                                        goal_word=self.words[self.goal_word])
+
+        reward = 0
+        if action == self.goal_word:
+            self.done = True
+            #reward = REWARD
+            if wordle.state.remaining_steps(self.state) == self.max_turns-1:
+                reward = 0#-10*REWARD  # No reward for guessing off the bat
+            else:
+                #reward = REWARD*(self.state.remaining_steps() + 1) / self.max_turns
+                reward = REWARD
+        elif dqn.state.remaining_steps(self.state) == 0:
+            self.done = True
+            reward = -REWARD
+
+        return self.state.copy(), reward, self.done
+    
